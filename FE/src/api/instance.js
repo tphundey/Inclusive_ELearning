0887dd1e0ffd26ccd46571e063bb263c9de39a80
Dinh Van Tai <105340771+bunbonhieuhanh@users.noreply.github.com>
2023-08-07 23:00:40 +0700

    chore: Delete package redux toolkit
    Thay đổi lại package sử dụng trong dự án bằng RTK Query

--- FE/src/api/instance.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-    baseURL: "http://localhost:3001",
-    headers: {
-        "Content-Type": "application/json"
-    }
-})
-export default instance
