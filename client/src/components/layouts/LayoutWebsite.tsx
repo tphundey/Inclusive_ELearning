f9921b5276be0f0f6bc5751b4e3343634e97d34e
Your Name <youremail@yourdomain.com>
2023-08-09 04:53:56 +0700

    ui: Add footer website #17 - Thêm footer cho trang web

--- client/src/components/layouts/LayoutWebsite.tsx
+++ client/src/components/layouts/LayoutWebsite.tsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom"
 import HeaderUser from "../HeaderUser";
-
+import FooterUser from "../FooterUser";
 const LayoutWebsite = () => {
     return <div>
         <HeaderUser />
         <Outlet />
+        <FooterUser />
     </div>;
 };
 
