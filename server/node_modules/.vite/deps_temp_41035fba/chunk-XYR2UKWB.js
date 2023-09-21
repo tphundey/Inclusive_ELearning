3e1936504462ab00a3e7b3814417141c556b6a9a
Your Name <youremail@yourdomain.com>
2023-09-21 17:19:20 +0700

    be: Create base NodeJs - Tạo base Nodejs thay thế Strapi

---
+++ server/node_modules/.vite/deps_temp_41035fba/chunk-XYR2UKWB.js
@@ -0,0 +1,21 @@
+import {
+  __commonJS
+} from "./chunk-7FP5O474.js";
+
+// browser-external:crypto
+var require_crypto = __commonJS({
+  "browser-external:crypto"(exports, module) {
+    module.exports = Object.create(new Proxy({}, {
+      get(_, key) {
+        if (key !== "__esModule" && key !== "__proto__" && key !== "constructor" && key !== "splice") {
+          console.warn(`Module "crypto" has been externalized for browser compatibility. Cannot access "crypto.${key}" in client code. See http://vitejs.dev/guide/troubleshooting.html#module-externalized-for-browser-compatibility for more details.`);
+        }
+      }
+    }));
+  }
+});
+
+export {
+  require_crypto
+};
+//# sourceMappingURL=chunk-XYR2UKWB.js.map
