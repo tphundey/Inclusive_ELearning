b330d5ff6a882e16fc7fdc8bec41933675d67f4e
datnspH <johndoe@example.comdatnsph25191@fpt.edu.vn>
2023-10-09 21:00:12 +0700

    be: update uploader in file index Routers - cập nhật thêm uploader trong file index Routers

--- server/src/Routers/index.js
+++ server/src/Routers/index.js
@@ -1,11 +1,12 @@
 import userRouter from "./user";
 import productRouter from './Course'
-import CategoryRouter from "./Categorys";
+import UploadRouter from "./uploader";
 import CartRouter from "./Cart";
 const routerApp = (app) => {
     app.use("/api", userRouter);
     app.use("/api", productRouter);
     app.use("/api", CategoryRouter);
+    app.use("/api", UploadRouter);
     app.use("/api", CartRouter);
 };
 
