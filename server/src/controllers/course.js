8105cea0b4b3b6b24bde1011a394ad7a8bd7b74c
1ongtimenosee <nongviettruong12@gmail.com>
2023-10-14 01:22:37 +0700

    be: Create controller course

--- server/src/controllers/course.js
+++ server/src/controllers/course.js
@@ -22,7 +22,7 @@ export const getAllCourse = async (req, res) => {
     },
   };
   try {
-    //seach du lieu
+    //tim kiem du lieu
     const searchData = (courseData) => {
       return courseData?.docs?.filter((item) =>
         item?.name?.toLowerCase().includes(_keyword)
