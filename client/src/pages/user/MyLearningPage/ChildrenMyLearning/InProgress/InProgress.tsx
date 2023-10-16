5be3e1557277e137b858ec9b85ea4d0b94da8180
Your Name <anhnek033@gmail.com>
2023-10-17 04:23:50 +0700

    feat: Get courseSaved - Lấy ra khóa học được lưu dựa theo email của người dùng trên local

--- client/src/pages/user/MyLearningPage/ChildrenMyLearning/InProgress/InProgress.tsx
+++ client/src/pages/user/MyLearningPage/ChildrenMyLearning/InProgress/InProgress.tsx
@@ -1,49 +1,121 @@
+import { useState, useEffect } from 'react';
+
+// Hàm giải mã dữ liệu sử dụng decodeURIComponent
+function decodeData(encryptedData: any): any {
+    return decodeURIComponent(encryptedData);
+}
+
 const InProgress = () => {
+    const [savedCourses, setSavedCourses] = useState([]);
+    const [email, setEmail] = useState(''); // Lưu email của người dùng
+    const [encryptedData, setEncryptedData] = useState<any>(null);
+
+    // Bước 1: Kiểm tra và giải mã dữ liệu từ localStorage (nếu có)
+    const encryptedProfile = localStorage.getItem('profile');
+    if (encryptedProfile) {
+        const decryptedProfile = decodeData(encryptedProfile);
+
+        // Bước 2: Chuyển thông tin đã giải mã thành đối tượng JSON
+        const profile = JSON.parse(decryptedProfile);
+
+        // Bước 3: Lấy email từ đối tượng profile
+        const userEmail = profile.email;
+
+        console.log('Email của người dùng:', userEmail);
+
+        // Bước 4: Sử dụng email trong truy vấn hoặc hiển thị
+    } else {
+        console.log('Không tìm thấy thông tin người dùng đã mã hóa trong Local Storage.');
+    }
+
+    useEffect(() => {
+        async function start() {
+            // Kiểm tra và giải mã dữ liệu từ localStorage (nếu có)
+            const encryptedProfile: any = localStorage.getItem('profile');
+            if (encryptedProfile) {
+                const decryptedProfile: any = decodeData(encryptedProfile);
+                console.log('Thông tin đã được giải mã từ localStorage:', decryptedProfile);
+                setEncryptedData(decryptedProfile);
+            }
+            else {
+                console.log('Ko chạy');
+            }
+        }
+        start();
+    }, []);
+
+    useEffect(() => {
+        // Bước 1: Kiểm tra và giải mã dữ liệu từ localStorage (nếu có)
+        const encryptedProfile = localStorage.getItem('profile');
+        if (encryptedProfile) {
+            const decryptedProfile = decodeData(encryptedProfile);
+            // Bước 2: Chuyển thông tin đã giải mã thành đối tượng JSON
+            const profile = JSON.parse(decryptedProfile);
+            // Bước 3: Lấy email từ đối tượng profile
+            const userEmail = profile.email;
+            console.log('Email của người dùng:', userEmail);
+
+            // Bước 4: Sử dụng email để lấy danh sách khóa học đã lưu
+            fetch(`http://localhost:3000/googleAccount?email=${userEmail}`)
+                .then((response) => response.json())
+                .then((userData) => {
+                    if (userData.length > 0) {
+                        const user = userData[0];
+                        // Bước 5: Lấy danh sách id khóa học đã lưu từ thông tin người dùng
+                        const savedCourseIds = user.courseSaved;
+
+                        // Bước 6: Thực hiện truy vấn để lấy danh sách khóa học từ cơ sở dữ liệu
+                        fetch(`http://localhost:3000/Courses`)
+                            .then((response) => response.json())
+                            .then((coursesData) => {
+                                // Bước 7: Lọc danh sách khóa học dựa trên id đã lưu
+                                const savedCourses = coursesData.filter((course) =>
+                                    savedCourseIds.includes(course.id)
+                                );
+                                // Bước 8: Lưu danh sách khóa học vào state
+                                setSavedCourses(savedCourses);
+                            })
+                            .catch((error) => {
+                                console.error('Error fetching courses: ', error);
+                            });
+                    }
+                })
+                .catch((error) => {
+                    console.error('Error fetching user data: ', error);
+                });
+        } else {
+            console.log('Không tìm thấy thông tin người dùng đã mã hóa trong Local Storage.');
+        }
+    }, []);
 
     return (
         <div className="listProgress">
-            <div className="courseProgress">
-                <div className="imgCoureProgress">
-                    <img src="https://media.licdn.com/dms/image/D560DAQF7oXTYu9_kTA/learning-public-crop_144_256/0/1689355790806?e=1692882000&v=beta&t=aBprtkKSsmhkYsuR1fMv2hoRRdAGp7HNktwQIUkMe2w" alt="" />
-                </div>
-                <div className="infoCourseProgress">
-                    <h3>COURSE</h3>
-                    <h2>After Effects: Using Expressions</h2>
-                    <div className="fl-info-progress">
-                        <div className="fl1-info-progress">
-                            <img src="https://media.licdn.com/dms/image/C560BAQHaVYd13rRz3A/company-logo_100_100/0/1638831589865?e=1700092800&v=beta&t=MFjcNX0hJDW7Goe5R8ZoAUHDj4B5d0UltcbssljghMY" alt="" />
+
+            {savedCourses.map((course: any) => (
+                <div>
+                    <div className="courseProgress">
+                        <div className="imgCoureProgress">
+                            <img src={course.courseIMG} alt="" />
                         </div>
-                        <div className="fl2-info-progress">
-                            LinkedIn - By: Luisa Winters - 1 month ago
+                        <div className="infoCourseProgress">
+                            <h3>COURSE</h3>
+                            <a href={`/introduction/${course.id}`}> <h2>{course.courseName}</h2></a>
+                            <div className="fl-info-progress">
+                                <div className="fl1-info-progress">
+                                    <img src="https://media.licdn.com/dms/image/C560BAQHaVYd13rRz3A/company-logo_100_100/0/1638831589865?e=1700092800&v=beta&t=MFjcNX0hJDW7Goe5R8ZoAUHDj4B5d0UltcbssljghMY" alt="" />
+                                </div>
+                                <div className="fl2-info-progress">
+                                    LinkedIn - By: Hương Trà - 1 month ago
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className="option-course-progress">
-                    <button><i className="fa-solid fa-ellipsis"></i></button>
-                </div>
-            </div>
-            <hr />
-            <div className="courseProgress">
-                <div className="imgCoureProgress">
-                    <img src="https://media.licdn.com/dms/image/C4E0DAQGjOIhc_dSnTQ/learning-public-crop_144_256/0/1591384433527?e=1692882000&v=beta&t=-Fxlc8vR8bkXGeevIXgR9WDmQ_it1zLe77bf7Lz3S6Y" alt="" />
-                </div>
-                <div className="infoCourseProgress">
-                    <h3>COURSE</h3>
-                    <h2>After Effects: Using Expressions</h2>
-                    <div className="fl-info-progress">
-                        <div className="fl1-info-progress">
-                            <img src="https://media.licdn.com/dms/image/C560BAQHaVYd13rRz3A/company-logo_100_100/0/1638831589865?e=1700092800&v=beta&t=MFjcNX0hJDW7Goe5R8ZoAUHDj4B5d0UltcbssljghMY" alt="" />
-                        </div>
-                        <div className="fl2-info-progress">
-                            LinkedIn - By: Luisa Winters - 1 month ago
+                        <div className="option-course-progress">
+                            <button><i className="fa-solid fa-ellipsis"></i></button>
                         </div>
                     </div>
+                    <hr />
                 </div>
-                <div className="option-course-progress">
-                    <button><i className="fa-solid fa-ellipsis"></i></button>
-                </div>
-            </div>
-            <hr />
+            ))}
         </div>
     )
 };
