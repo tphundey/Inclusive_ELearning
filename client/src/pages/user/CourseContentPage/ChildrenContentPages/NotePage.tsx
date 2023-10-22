4316ffb4db83ae390f6da01c61c61a8b6b5b1e17
Your Name <anhnek033@gmail.com>
2023-10-22 19:02:53 +0700

    feat: Post note for course - Hỗ trợ date post theo múi giờ VietNam

--- client/src/pages/user/CourseContentPage/ChildrenContentPages/NotePage.tsx
+++ client/src/pages/user/CourseContentPage/ChildrenContentPages/NotePage.tsx
@@ -1,18 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import moment from 'moment-timezone';
 
 function NotesComponent() {
     const [notes, setNotes] = useState<any[]>([]);
     const [editingNote, setEditingNote] = useState(null);
     const [editingNoteContent, setEditingNoteContent] = useState('');
     const [originalNoteContent, setOriginalNoteContent] = useState(''); // Thêm trạng thái để lưu nội dung cũ
-
+    const [newNoteContent, setNewNoteContent] = useState(''); // Thêm trạng thái để lưu nội dung ghi chú mới
     const { id } = useParams();
+    const parsedVideoID = parseInt(id, 10);
+    const [newNoteDate, setNewNoteDate] = useState('');
+
     useEffect(() => {
         axios.get(`http://localhost:3000/Notes?videoID=${id}`)
             .then((response) => {
                 const notesData = response.data;
+                // Sắp xếp danh sách ghi chú theo trường date giảm dần (mới nhất đầu tiên)
+                notesData.sort((a, b) => new Date(b.date) - new Date(a.date));
                 setNotes(notesData);
             })
             .catch((error) => {
@@ -40,13 +46,24 @@ function NotesComponent() {
     };
     // Hàm xử lý khi nhấn nút lưu chỉnh sửa
     const handleSaveEdit = (noteId) => {
+        // Tìm ghi chú cần chỉnh sửa
+        const noteToEdit = notes.find((note) => note.id === noteId);
+
+        // Lấy thời gian hiện tại dựa theo múi giờ Việt Nam
+        const vietnamTime = moment.tz('Asia/Ho_Chi_Minh');
+        const formattedDate = vietnamTime.format('YYYY-MM-DD');
+
         // Gửi yêu cầu chỉnh sửa ghi chú đến API
-        axios.put(`http://localhost:3000/Notes/${noteId}`, { note: editingNoteContent }) // Sửa thành trường 'note'
+        axios.put(`http://localhost:3000/Notes/${noteId}`, {
+            videoID: noteToEdit.videoID,
+            note: editingNoteContent,
+            date: formattedDate // Cập nhật trường date thành thời gian hiện tại
+        })
             .then((response) => {
                 // Sau khi chỉnh sửa thành công, cập nhật trạng thái danh sách ghi chú với nội dung mới
                 setNotes((prevNotes) =>
                     prevNotes.map((note) =>
-                        note.id === noteId ? { ...note, note: editingNoteContent } : note
+                        note.id === noteId ? { ...note, note: editingNoteContent, date: formattedDate } : note
                     )
                 );
                 // Kết thúc chỉnh sửa
@@ -56,18 +73,54 @@ function NotesComponent() {
                 console.error('Lỗi khi chỉnh sửa ghi chú:', error);
             });
     };
+    // Hàm xử lý khi người dùng nhập ghi chú mới và nhấn Enter
+    const handleNewNoteEnter = (e) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
 
+            // Lấy thời gian hiện tại dựa theo múi giờ Việt Nam
+            const vietnamTime = moment.tz('Asia/Ho_Chi_Minh');
+            const formattedDate = vietnamTime.format('YYYY-MM-DD');
+
+            // Gửi yêu cầu POST để tạo ghi chú mới
+            axios.post('http://localhost:3000/Notes', {
+                videoID: parsedVideoID,
+                note: newNoteContent,
+                date: formattedDate
+            })
+                .then((response) => {
+                    // Thêm ghi chú mới vào đầu danh sách
+                    setNotes([response.data, ...notes]);
+                    // Xóa nội dung trường nhập liệu
+                    setNewNoteContent('');
+                })
+                .catch((error) => {
+                    console.error('Lỗi khi tạo ghi chú mới:', error);
+                });
+        }
+    };
     return (
         <div className="note-page-content">
             <div className="notepage-left">
-                <textarea placeholder="Type your note here to save for later..." name="" id="" aria-colspan={30} aria-rowindex={10}></textarea>
+                <div>
+                    <textarea
+                        placeholder="Type your note here to save for later..."
+                        name=""
+                        id=""
+                        aria-colspan={30}
+                        aria-rowindex={10}
+                        value={newNoteContent}
+                        onChange={(e) => setNewNoteContent(e.target.value)}
+                        onKeyPress={handleNewNoteEnter}
+                    />
+                </div>
                 <div className="note-fl-info">
                     <p>{notes.length} Notes taken</p>
                     <p>Press Enter to save</p>
                 </div>
                 {notes.map((note) => (
                     <div className="note-user-content" key={note.id}>
-                        <h2><i className="fa-solid fa-bookmark mr-2"></i>1</h2>
+                        <h2><i className="fa-solid fa-bookmark mr-2"></i>{note.date}</h2>
                         {editingNote === note.id ? (
                             <div>
                                 <textarea
