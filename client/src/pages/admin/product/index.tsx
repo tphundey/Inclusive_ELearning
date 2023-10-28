e76eb410d23402d88aa3e12e9a8cb491efc935c8
Your Name <anhnek033@gmail.com>
2023-10-28 10:52:34 +0700

    feat: Function to display course details

--- client/src/pages/admin/product/index.tsx
+++ client/src/pages/admin/product/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Table, Skeleton, Popconfirm, message, Pagination } from 'antd';
+import { Button, Table, Skeleton, Popconfirm, message, Pagination, Modal } from 'antd';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
@@ -8,7 +8,11 @@ const AdminProduct = () => {
     const [productsData, setProductsData] = useState([]);
     const [isProductLoading, setIsProductLoading] = useState(true);
     const [currentPage, setCurrentPage] = useState(1);
-
+    const [showFullDescription, setShowFullDescription] = useState(false);
+    const [selectedCourseVideos, setSelectedCourseVideos] = useState([]);
+    const [isVideosModalVisible, setIsVideosModalVisible] = useState(false);
+    const [selectedCourseReviews, setSelectedCourseReviews] = useState([]);
+    const [isReviewsModalVisible, setIsReviewsModalVisible] = useState(false);
     const pageSize = 3;
 
 
@@ -33,6 +37,49 @@ const AdminProduct = () => {
         fetchData();
     }, []);
 
+    const showCourseVideos = (courseID: any) => {
+        axios.get('http://localhost:3000/Courses')
+            .then((response) => {
+                const selectedCourse = response.data.find((course) => course.id === courseID);
+
+                if (selectedCourse) {
+                    const videoIDs = selectedCourse.videoID;
+                    if (videoIDs && videoIDs.length > 0) {
+                        axios.get('http://localhost:3000/videos')
+                            .then((videoResponse) => {
+                                const selectedVideos = videoResponse.data.filter((video) => {
+                                    return videoIDs.includes(video.id);
+                                });
+
+                                // Gán dữ liệu video vào biến selectedCourseVideos
+                                setSelectedCourseVideos(selectedVideos);
+
+                                // Hiển thị modal sau khi đã có dữ liệu
+                                setIsVideosModalVisible(true);
+                            })
+                            .catch((error) => {
+                                console.error('Lỗi khi lấy danh sách video: ', error);
+                            });
+                    }
+                }
+            })
+            .catch((error) => {
+                console.error('Lỗi khi lấy danh sách khóa học: ', error);
+            });
+    };
+
+
+    const showCourseReviews = (courseID: any) => {
+        // Gửi yêu cầu API để lấy danh sách đánh giá dựa trên courseID
+        axios.get(`http://localhost:3000/Reviews?courseID=${courseID}`)
+            .then((response) => {
+                setSelectedCourseReviews(response.data);
+                setIsReviewsModalVisible(true);
+            })
+            .catch((error) => {
+                console.error('Lỗi khi lấy danh sách đánh giá: ', error);
+            });
+    };
 
     const columns = [
         {
@@ -42,15 +89,34 @@ const AdminProduct = () => {
             width: '200px',
         },
         {
-            title: 'Giá',
+            title: 'Giá (VND)',
             dataIndex: 'price',
             key: 'price',
             width: '100px',
         },
         {
-            title: 'Mô tả ngắn',
-            dataIndex: 'desc',
+            title: 'Mô tả khóa học',
+            dataIndex: 'description',
             key: 'desc',
+            width: '450px',
+            render: (description: string) => {
+                const shortDescription = showFullDescription
+                    ? description
+                    : description.length > 100
+                        ? `${description.slice(0, 100)}...`
+                        : description;
+
+                return (
+                    <>
+                        <p>{shortDescription}</p>
+                        {description.length > 100 && (
+                            <Button type="link" onClick={() => setShowFullDescription(!showFullDescription)}>
+                                {showFullDescription ? 'Rút gọn' : 'Xem thêm'}
+                            </Button>
+                        )}
+                    </>
+                );
+            },
         },
         {
             title: 'Ngày phát hành',
@@ -85,7 +151,11 @@ const AdminProduct = () => {
                         </Popconfirm>
                         <Button className='ml-2'>
                             <Link to={`/admin/product/${record.id}/edit`}>Sửa</Link>
-                        </Button></>
+                        </Button>
+                        <Button className='ml-2' onClick={() => showCourseVideos(record.id, showCourseReviews(record.id))}>
+                            <i className="fa-solid fa-bars"></i>
+                        </Button>
+                    </>
                 );
             },
         },
@@ -96,7 +166,7 @@ const AdminProduct = () => {
     const endItem = currentPage * pageSize;
     const currentData = productsData.slice(startItem, endItem);
 
-    const updateHiddenState = (productId:any, isHidden:any) => {
+    const updateHiddenState = (productId: any, isHidden: any) => {
 
         const updatedHiddenState = { isHidden: isHidden };
 
@@ -119,9 +189,38 @@ const AdminProduct = () => {
     };
 
 
-
     return (
         <div>
+            <Modal
+                title="Chi tiết khóa học"
+                visible={isVideosModalVisible}
+                onOk={() => setIsVideosModalVisible(false)}
+                onCancel={() => setIsVideosModalVisible(false)}
+            >
+                <ul>
+                    {selectedCourseVideos.map((video, index) => (
+                        <li key={index}>
+                            <a href={video.videoURL} target="_blank" rel="noopener noreferrer">
+                                {video.videoTitle}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+                <br />
+                <hr />
+                <br />
+                <ul>
+                    {selectedCourseReviews.map((review) => (
+                        <li key={review.id}>
+                            <div>Rate: {review.rating} <i class="fa-regular fa-star"></i></div>
+                            <div>Bình luận: {review.comment}</div>
+                            <div>Ngày: {review.date}</div>
+                            <br />
+                        </li>
+
+                    ))}
+                </ul>
+            </Modal>
             <header className="flex items-center justify-between mb-4">
                 <h2 className="text-2xl">Quản lý Khóa Học</h2>
                 <Button type="primary" danger>
