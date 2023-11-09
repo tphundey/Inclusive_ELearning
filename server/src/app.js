2aa08de0fd2d8c8864e55d90588e183b5aa28774
datnspH <johndoe@example.comdatnsph25191@fpt.edu.vn>
2023-11-09 17:02:23 +0700

    be: update router

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import express from "express";
-// import routerApp from "./router";
-import CourseRouter from "./routers/Course";
+import routerApp from "./Routers/index";
+// import CourseRouter from "./routers/Course";
 import authRouter from "./Routers/auth";
 import categoryRouter from "./Routers/Categorys";
 import videoRouter from './Routers/video';
@@ -13,18 +13,18 @@ import cors from 'cors';
 const app = express();
 app.use(express.json());
 app.use(cors());
-// routerApp(app);
+routerApp(app);
 
 app.get("/", (req, res) => {
     res.send("Hello word");
 });
-app.use('/', CourseRouter);
-app.use('/',videoRouter);
-app.use('/', authRouter)
-app.use('/', categoryRouter)
-app.use('/',userRouter)
-app.use('/',upload)
-app.use('/api', payments);
+// app.use('/', CourseRouter);
+// app.use('/',videoRouter);
+// app.use('/', authRouter)
+// app.use('/', categoryRouter)
+// app.use('/',userRouter)
+// app.use('/',upload)
+// app.use('/api', payments);
 
 
 mongoose.connect('mongodb+srv://datnsph25191:lvmSjx4T4CvRDckN@cluster0.4xubugv.mongodb.net/').then(
