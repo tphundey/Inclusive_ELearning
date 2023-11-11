c97dab51f75a6482aa70047242c886d4ab5319ab
datnspH <johndoe@example.comdatnsph25191@fpt.edu.vn>
2023-11-11 15:45:15 +0700

    update-router

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,6 @@
 import mongoose from "mongoose";
 import express from "express";
 import routerApp from "./Routers/index";
-// import CourseRouter from "./routers/Course";
-import authRouter from "./Routers/auth";
-import categoryRouter from "./Routers/Categorys";
-import videoRouter from './Routers/video';
-import userRouter from './Routers/user'
-import upload from './Routers/uploader'
-import payments from "./Routers/payments";
 import cors from 'cors';
 
 const app = express();
@@ -18,13 +11,6 @@ routerApp(app);
 app.get("/", (req, res) => {
     res.send("Hello word");
 });
-// app.use('/', CourseRouter);
-// app.use('/',videoRouter);
-// app.use('/', authRouter)
-// app.use('/', categoryRouter)
-// app.use('/',userRouter)
-// app.use('/',upload)
-// app.use('/api', payments);
 
 
 mongoose.connect('mongodb+srv://datnsph25191:lvmSjx4T4CvRDckN@cluster0.4xubugv.mongodb.net/').then(
