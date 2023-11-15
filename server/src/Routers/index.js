df516e62504d299d9dc218ada2f0b7a4f9036268
datnspH <johndoe@example.comdatnsph25191@fpt.edu.vn>
2023-11-15 21:52:43 +0700

    update env

--- server/src/Routers/index.js
+++ server/src/Routers/index.js
@@ -1,13 +1,17 @@
 import userRouter from "./user";
-import productRouter from './Course'
+import productRouter from "./Course";
 import UploadRouter from "./uploader";
-import CartRouter from "./Cart";
+// import CartRouter from "./Cart";
+import paymentsRouter from "./payments";
+import categoryRouter from "./Categorys";
+
 const routerApp = (app) => {
     app.use("/api", userRouter);
     app.use("/api", productRouter);
-    app.use("/api", CategoryRouter);
+    app.use("/api", categoryRouter);
     app.use("/api", UploadRouter);
-    app.use("/api", CartRouter);
+    // app.use("/api", CartRouter);
+    app.use("/api", paymentsRouter);
 };
 
 export default routerApp;
--- server/src/app.js
+++ server/src/app.js
@@ -1,30 +1,16 @@
 import mongoose from "mongoose";
 import express from "express";
-// import routerApp from "./router";
-import CourseRouter from "./routers/Course";
-import authRouter from "./Routers/auth";
-import categoryRouter from "./Routers/Categorys";
-import videoRouter from './Routers/video';
-import userRouter from './Routers/user'
-import upload from './Routers/uploader'
-import payments from "./Routers/payments";
+import routerApp from "./Routers";
 import cors from 'cors';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-// routerApp(app);
+routerApp(app);
 
 app.get("/", (req, res) => {
     res.send("Hello word");
 });
-app.use('/', CourseRouter);
-app.use('/',videoRouter);
-app.use('/', authRouter)
-app.use('/', categoryRouter)
-app.use('/',userRouter)
-app.use('/',upload)
-app.use('/api', payments);
 
 
 mongoose.connect('mongodb+srv://datnsph25191:lvmSjx4T4CvRDckN@cluster0.4xubugv.mongodb.net/').then(
