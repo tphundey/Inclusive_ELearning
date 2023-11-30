fabc0c2becdcfb78668c648c606891c29359d0c2
Your Name <taidvph20044@gmail.com>
2023-11-30 15:35:44 +0700

    Update user role based on UID

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -53,7 +53,7 @@ let userRole: 0 | 1 = 0;
 
 const uid = getUidFromLocalStorage();
 
-if (uid === "2o2RPub7yJd9liwygYQJxTA98uV2") {
+if (uid === "z6pdJJzcVoZM8RX5ZrQFqh6UDWL2")   {
     userRole = 1;
 }
 
