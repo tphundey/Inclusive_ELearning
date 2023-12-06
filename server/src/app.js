a409c598b63886bdde326024ccdd625230203f0c
Your Name <taidvph20044@gmail.com>
2023-12-06 10:38:21 +0700

    update router for all

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,30 @@
 import mongoose from "mongoose";
 import express from "express";
 import cors from 'cors';
-import router from './controllers/course'
+import router from "./controllers/course"
 import routerCate from "./controllers/categories";
+import routerGoogleAccounts from "./controllers/googleAccount";
+import routerNotes from "./controllers/note";
+import routerPayments from "./controllers/payment";
+import routerPosts from "./controllers/posts";
+import routerReviews from "./controllers/review";
+import routerUserProgress from "./controllers/userProgress";
+import routerVideoProgress from "./controllers/userVideoProgress";
+import routerVideo from "./controllers/video";
+
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use("/courses", router);
-app.use("/categories", routerCate);
+app.use("/Courses", router);
+app.use("/Categories", routerCate);
+app.use("/googleAccount", routerGoogleAccounts);
+app.use("/Payment", routerPayments);
+app.use("/posts", routerPosts);
+app.use("/Reviews", routerReviews);
+app.use("/UserProgress", routerUserProgress);
+app.use("/userVideoProgress", routerVideoProgress);
+app.use("/Videos", routerVideo);
+app.use("/Notes", routerNotes);
 
 app.get("/", (req, res) => {
     res.send("Hello world");
