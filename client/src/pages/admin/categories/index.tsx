b54ee759ffce0b9e418f616b97ec3d98442674d8
Your Name <taidvph20044@gmail.com>
2023-12-09 16:23:45 +0700

    check course when delete category

--- client/src/pages/admin/categories/index.tsx
+++ client/src/pages/admin/categories/index.tsx
@@ -1,28 +1,64 @@
+import { useEffect } from 'react';
 import { useGetCategorysQuery, useRemoveCategoryMutation } from "@/api/category";
 import { Icategory } from "@/interfaces/category";
 import { Button, Table, Skeleton, Popconfirm, message } from "antd";
 import { Link } from "react-router-dom";
-
+import axios from 'axios';
 const AdminCategory = (props: any) => {
     const [messageApi, contextHolder] = message.useMessage();
     const { data: productsData, isLoading: isProductLoading } = useGetCategorysQuery();
     const [removeCategory, { isLoading: isRemoveLoading }] = useRemoveCategoryMutation();
+
+
     const dataSource = productsData?.map((item: Icategory) => ({
         key: item.id,
         categoryName: item.categoryName,
         categoryDescription: item.categoryDescription
     }));
-    console.log(dataSource);
 
-    const confirm = (id:string) => {
-        removeCategory(id)
-            .unwrap()
-            .then(() => {
+
+
+    const checkCoursesInCategory = async (categoryId: string) => {
+        try {
+            // Lấy danh sách khóa học từ API
+            const response = await axios.get('http://localhost:3000/Courses');
+            const courses = response.data;
+
+            // Kiểm tra xem có khóa học nào có categoryId trùng với id của category đang định xóa hay không
+            const coursesInCategory = courses.filter((course: any) => course.categoryID === categoryId);
+            console.log(coursesInCategory);
+
+            if (coursesInCategory.length > 0) {
+                // Có khóa học trong danh mục, hiển thị cảnh báo và return false
                 messageApi.open({
-                    type: "success",
-                    content: "Deleted successfully!",
+                    type: "warning",
+                    content: "Danh mục này có khóa học. Vui lòng xóa khóa học trước khi xóa danh mục.",
                 });
-            });
+                return false;
+            }
+
+            // Không có khóa học trong danh mục, return true
+            return true;
+        } catch (error) {
+            console.error("Error checking courses in category:", error);
+            return false; // Xử lý lỗi và không xóa danh mục
+        }
+    };
+
+    const confirm = (id: string) => {
+ 
+        checkCoursesInCategory(id).then((canRemove) => {
+            if (canRemove) {
+                removeCategory(id)
+                    .unwrap()
+                    .then(() => {
+                        messageApi.open({
+                            type: "success",
+                            content: "Deleted successfully!",
+                        });
+                    });
+            }
+        });
     };
 
     const columns = [
@@ -38,7 +74,7 @@ const AdminCategory = (props: any) => {
         },
         {
             title: "Hành động",
-            render: ({ key: id }: { key:string }) => (
+            render: ({ key: id }: { key: string }) => (
                 <div className="flex space-x-2">
                     <Popconfirm
                         placement="top"
@@ -49,7 +85,7 @@ const AdminCategory = (props: any) => {
                         cancelText="No"
                     >
                         <Button type="primary" danger>
-                        <i className="fa-solid fa-trash"></i>
+                            <i className="fa-solid fa-trash"></i>
                         </Button>
                     </Popconfirm>
                     <Button>
